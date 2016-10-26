8270b29ac7af2abd70d3f723062ce8d5d2cbc0ef
Franki2210 <idimka2210@gmail.com>
2016-10-26 09:34:04 +0300

    Добавлены тесты

--- lab1/flipbyte/test.bat
+++ lab1/flipbyte/test.bat
@@ -6,11 +6,15 @@ fc.exe %TEMP%\output trueResult.txt
 if ERRORLEVEL 1 goto err
 
 rem проверка на число за пределами 0..255
-%PROGRAM% 266
+%PROGRAM% 266 > %TEMP%\output
 if NOT ERRORLEVEL 1 goto err
+fc.exe %TEMP%\output beyond.txt
+if ERRORLEVEL 1 goto err
 
-rem проверка на 0
-%PROGRAM% 0
+rem проверка с нулём
+%PROGRAM% 0 > %TEMP%\output
+if ERRORLEVEL 1 goto err
+fc.exe %TEMP%\output trueResultZero.txt
 if ERRORLEVEL 1 goto err
 
 rem проверка на число на границе 0..255
